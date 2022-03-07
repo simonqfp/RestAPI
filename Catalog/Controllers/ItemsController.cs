c48b37d1e608a833370edde20ed89adffeb87bc8
Simon Kirkley <simon.kirkley@quilter.com>
2022-03-07 08:29:13 +0000

    initial commit

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,101 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Catalog.Repositories;
+using System.Collections.Generic;
+using Catalog.Entities;
+using System;
+using System.Linq;
+using Catalog.Dtos;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+
+        private readonly IItemsRepository repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+
+        // GET /items
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            var items = repository.GetItems().Select(item => item.AsDto());
+
+            return items;
+
+        }
+
+        // GET /itms/{id}
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return item.AsDto();
+        }
+
+        // 
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+
+            };
+
+            repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
+        }
+
+        // PUT /items/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            var updatedItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+
+            repository.UpdateItem(updatedItem);
+            return NoContent();
+        }
+
+        // DELETE /items/delete/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            repository.DeleteItem(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ Catalog/Dtos/UpdateItemDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Dtos
+{
+    public record UpdateItemDto
+    {
+        [Required]
+
+        public string Name { get; init; }
+
+        [Required]
+        [Range(1, 1000)]
+        public decimal Price { get; set; }
+
+
+    }
+}
