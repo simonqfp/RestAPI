1312685ab37d770c874aea9bd87029f140b234c2
simonki73 <39844187+simonki73@users.noreply.github.com>
2022-03-08 08:46:08 +0000

    started to add mongodb

---
+++ Catalog/Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Repositories
+{
+
+
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+        private const string databaseName = "Catalog";
+        private const string collectionName = "Items";
+
+        private readonly IMongoCollection<Item> itemsCollection;
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+           IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+           itemsCollection = database.GetCollection<Item>(collectionName);
+        }
+        public void CreateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
