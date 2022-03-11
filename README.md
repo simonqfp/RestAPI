a58d08c9fe00b28d29c6b8db4ba5335eda1d3671
simonki73 <39844187+simonki73@users.noreply.github.com>
2022-03-11 16:28:21 +0000

    Updated docker scripts

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # RestAPI
-https://youtu.be/ZXdFisA_hOY?t=7956
+https://youtu.be/ZXdFisA_hOY?t=9502
 
 # Create MondoDB on Docker Desktop for Windows
 https://blog.jeremylikness.com/blog/2018-12-27_mongodb-on-windows-in-minutes-with-docker/
--- Scripts/docker.ps1
+++ Scripts/docker.ps1
@@ -1,8 +1,15 @@
+docker stop mongo
+docker volume ls
+docker volume rm mongodbdata
+
+docker ps
+
+
 # create a docker volume for our MongoDb image
 docker volume create --name=mongodata
 
 # mount and start start our MongoDb image
-docker run --name mongodb -v mongodata:/data/db -d -p 27017:27017 mongo
+docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=Password123! mongo
 #docker run -d --rm mongo -p 27017:27017 -v mongodbdata:/data/db mongo
 
 # open firewall for MongoDB port
@@ -10,3 +17,8 @@ netsh advfirewall firewall add rule name="Open mongod port 27017" dir=in action=
 
 # open firewall for MongoDb exe
 netsh advfirewall firewall add rule name="Allowing mongod" dir=in action=allow program=" C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe"
+
+# user secrets
+dotnet user-secrets init
+
+dotnet user-secrets set MongoDbSettings:Password Password123!
