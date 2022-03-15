5a43d3de58c771e44c5add038504132015c66346
simonki73 <39844187+simonki73@users.noreply.github.com>
2022-03-15 09:40:22 +0000

    Finshed off health checks

--- Catalog/Startup.cs
+++ Catalog/Startup.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Catalog.Repositories;
 using Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -55,10 +58,10 @@ namespace Catalog
             });
 
             services.AddHealthChecks().AddMongoDb(
-                mongoDbSettings.ConnectionString, 
-                name: "mongodb", 
-                timeout: TimeSpan.FromSeconds(3), 
-                tags: new[]{"ready"});
+                mongoDbSettings.ConnectionString,
+                name: "mongodb",
+                timeout: TimeSpan.FromSeconds(3),
+                tags: new[] { "ready" });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -80,12 +83,32 @@ namespace Catalog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                
-                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions{
-                    Predicate = (check) => check.Tags.Contains("ready")
+
+                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+                {
+                    Predicate = (check) => check.Tags.Contains("ready"),
+                    ResponseWriter = async (context, report) =>
+                    {
+                        var result = JsonSerializer.Serialize(
+                            new
+                            {
+                                status = report.Status.ToString(),
+                                checks = report.Entries.Select(entry => new
+                                {
+                                    name = entry.Key,
+                                    status = entry.Value.Status.ToString(),
+                                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message.ToString() : "none",
+                                    duration = entry.Value.Duration.ToString()
+                                })
+                            }
+                        );
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
                 });
 
-                endpoints.MapHealthChecks("/health/live", new HealthCheckOptions{
+                endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+                {
                     Predicate = (_) => false
                 });
 
